54461ba8dfa971efd94aa49e49b77ce9ba9a9184
Tristan Young <tyoung22@turing.edu>
2023-02-01 13:15:27 -0800

    Reassign inStock variable

--- src/warm-up.js
+++ src/warm-up.js
@@ -12,11 +12,16 @@
 // var food = "Ice Cream";
 // var type = "Dessert";
 // var inStock = 10
+var food = "Ramen";
+var type = "Entree";
+var inStock = 10; 
 
 
 
 // Reassign (not initialize) your "inStock" variable from above. We got a new shipment! Use a built in math operator to make 
 // the inStock count double what it currently is.
+inStock = inStock*2
+console.log(inStock)
 
 
 // Reassign (not initialize) your "inStock" variable from above. You got hungry and needed a snack! 
