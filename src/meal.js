e77a96bbf5514b441cd1fa7aaf6f81d24d2222b2
Tristan Young <tyoung22@turing.edu>
2023-02-01 20:56:53 -0800

    Add functions nameMenuItem, createMenuItem, AddIngredients

--- src/meal.js
+++ src/meal.js
@@ -1,12 +1,32 @@
-function nameMenuItem() {
-
+function nameMenuItem(name) {
+  var menuItemName = 'Delicious ' + name
+  return menuItemName
 }
+// console.log(nameMenuItem('Pizza'))
+
+
+function createMenuItem() {
+  var menuItem = {
+    name: 'Delicious French Toast',
+    price: 10.99,
+    type: 'breakfast'
+  }
+  return menuItem
+}
+// console.log(createMenuItem());
+
+function addIngredients(name) {
+  var ingredients = ['Cheese'] 
+  return ingredients
+}
+console.log(addIngredients());
+
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
+  createMenuItem,
+  addIngredients,
   // formatPrice,
   // decreasePrice,
   // createRecipe
