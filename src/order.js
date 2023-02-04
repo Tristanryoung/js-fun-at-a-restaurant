af746a41362b6adf26795e38f47fe3f9611d5f4e
Tristan Young <tyoung22@turing.edu>
2023-02-03 18:41:07 -0800

    Add functions takeOrder and refundOrder

--- src/order.js
+++ src/order.js
@@ -1,7 +1,28 @@
+function takeOrder(order, array) {
+  if(array.length<=2) {
+    array.push(order)
+  }
+}
+
+// function refundOrder(order, array) {
+//   array.shift(order)
+// }
+function refundOrder(order, array) {
+  for(var i = 0; i < array.length; i++) {
+  if(array[i].orderNumber === order) {
+    return array.splice([i], 1)
+  }
+    }
+  }
+  // console.log(refundOrder(2893, deliveryOrders))
+ 
+
+
+
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
+  takeOrder,
+  refundOrder,
   // listItems,
   // searchOrder
 }
