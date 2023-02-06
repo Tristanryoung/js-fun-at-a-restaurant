b0f6e482522bc6274f7d6799abc7d55416c94045
Tristan Young <tyoung22@turing.edu>
2023-02-05 19:01:18 -0800

    Add functions createRestaurant and addMenuItem

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,38 @@
+function createRestaurant(restaurantName) {
+  var pizzaRestaurant = {
+    name: restaurantName,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: []
+    }
+  }
+  return pizzaRestaurant
+}
+
+// function addMenuItem(pizzaRestaurant, menuItem) {
+//   // var menuItem = pizzaRestaurant.menus.lunch.unshift(menuItem)
+//   // if(pizzaRestaurant.menus.lunch.length ===1) {
+//   //   pizzaRestaurant.menus.lunch.push(menuItem)
+//   // }
+// }
+
+function addMenuItem(pizzaRestaurant, menuItem) {
+  // var menuItem = pizzaRestaurant.menus.lunch.push(menuItem)
+  if(menuItem.type === "breakfast" && !pizzaRestaurant.menus.breakfast.includes(menuItem)) {
+    pizzaRestaurant.menus.breakfast.push(menuItem)
+  } else if(menuItem.type === "lunch" && !pizzaRestaurant.menus.lunch.includes(menuItem)) {
+    pizzaRestaurant.menus.lunch.push(menuItem)
+  } else if(menuItem.type === "dinner" && !pizzaRestaurant.menus.dinner.includes(menuItem)) {
+    pizzaRestaurant.menus.dinner.push(menuItem)
+  }
+  console.log('bananas', pizzaRestaurant.menus)
+}
+
 
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
+  createRestaurant, 
+  addMenuItem,
   // removeMenuItem
 }
